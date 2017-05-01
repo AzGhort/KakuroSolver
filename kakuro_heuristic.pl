b54022521ab6f11bb287b875ff0a83d546779987
AzGhort <ghort@seznam.cz>
2017-05-01 14:54:55 +0200

    Add files via upload

--- kakuro_heuristic.pl
+++ kakuro_heuristic.pl
@@ -1,10 +1,11 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%user interaction%%%%%
+%%% user interaction %%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%
 
 %+IndexOfProblem, +X - Board and its index to be printed
 output(IndexOfProblem,X) :-
-    writeBoardLineByLine(IndexOfProblem,X).
+    writeBoardLineByLine(IndexOfProblem,X),
+    fail.
 
 %following methods only format board output
 writeBoardLineByLine(_,[]).
@@ -37,14 +38,20 @@ kakuro(a1, [[o, control(2,1),control(3,1)],[control(1,4),_,_],[control(1,5),_,_]
 %problem no.2, 4x4
 kakuro(2, [[o, control(2,1),control(3,1),control(4,1)],[control(1,5),_,_,_],[control(1,6),_,_,_],[control(1,7),_,_,_]]) :-
     fillHintsWithNumbers(2, [[o, control(2,1),control(3,1),control(4,1)],[control(1,5),_,_,_],[control(1,6),_,_,_],[control(1,7),_,_,_]]).
+%third part, 5x5
+%problem no.3, 5x5
+kakuro(3, [[o, o, control(2,1),control(3,1),o],[o,control(4,5),_,_,control(6,1)],[control(1,7),_,_,_,_],[control(1,8),_,_,_,_],[o,control(1,9),_,_,o]]) :-
+    fillHintsWithNumbers(3, [[o, o, control(2,1),control(3,1),o],[o,control(4,5),_,_,control(6,1)],[control(1,7),_,_,_,_],[control(1,8),_,_,_,_],[o,control(1,9),_,_,o]]).
 
 hints(1,[[oo],[13,[2,2],[3,2]],[9,[2,3],[3,3]],[17,[2,2],[2,3]],[5,[3,2],[3,3]]]).
 hints(a1,[[oo],[11,[2,2],[3,2]],[6,[2,3],[3,3]],[13,[2,2],[2,3]],[4,[3,2],[3,3]]]).
 hints(2,[[oo],[17,[2,2],[3,2],[4,2]],[12,[2,3],[3,3],[4,3]],[10,[2,4],[3,4],[4,4]],[23,[2,2],[2,3],[2,4]],[9,[3,2],[3,3],[3,4]],[7,[4,2],[4,3],[4,4]]]).
+hints(3, [[oo],[19,[2,3],[3,3],[4,3],[5,3]],[11,[2,4],[3,4],[4,4],[5,4]],[14,[3,2],[4,2]],[9,[2,3],[2,4]],[5,[3,5],[4,5]],[11,[3,2],[3,3],[3,4],[3,5]],[26,[4,2],[4,3],[4,4],[4,5]],[3,[5,3],[5,4]]]).
 
 hintsFreeCells(1, [[13,_,_],[9,_,_],[17,_,_],[5,_,_]]).
 hintsFreeCells(a1, [[11,_,_],[6,_,_],[13,_,_],[4,_,_]]).
 hintsFreeCells(2,[[17,_,_,_],[12,_,_,_],[10,_,_,_],[23,_,_,_],[9,_,_,_],[7,_,_,_]]).
+hintsFreeCells(3,[[19,_,_,_,_],[11,_,_,_,_],[14,_,_],[9,_,_],[5,_,_],[11,_,_,_,_],[26,_,_,_,_],[3,_,_]]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%% "AI" itself %%%%%%%%%
@@ -53,47 +60,43 @@ hintsFreeCells(2,[[17,_,_,_],[12,_,_,_],[10,_,_,_],[23,_,_,_],[9,_,_,_],[7,_,_,_
 fillHintsWithNumbers(IndexOfProblem, Board) :-
     hintsFreeCells(IndexOfProblem, List),
     hints(IndexOfProblem, [_|Hints]),
-    fillHints(List),
-    unifyBoard(List, Hints, Board, NewBoard),
+    fillHints(List, Hints, Board, NewBoard),
     solve(IndexOfProblem, NewBoard).
 
 %recursively fills in hints, one by one
-fillHints([]).
-fillHints([Head|Rest]) :-
-    fillHint(Head),
-    fillHints(Rest).
+fillHints([], _, Board, Board).
+fillHints([Head|Rest], [HeadHint|Hints], Board, NewBoard) :-
+    fillHint(Head, HeadHint, Board, CurBoard),
+    fillHints(Rest, Hints, CurBoard, NewBoard).
 
 %fills one specific hint in format [X,_,_,...]
-fillHint([Sum|Rest]) :-
-    fillRestHints(Sum, 0, Rest, Rest).
+fillHint([Sum|Rest], [Sum|RestHint], Board, NewBoard) :-
+    fillRestHints(Sum, 0, [], Rest, RestHint, Board, NewBoard).
 
 % the main "heuristic" part - only inserts unique numbers, always checks
 % if currentSum is less than final Sum
 % if there is only one element left, it is unified with Sum - CurrentSum
-fillRestHints(Sum, IncompleteSum, CompleteHint,[NextElement|Rest]) :-
-    Rest = [] ->                                 %last element of the hint
+fillRestHints(_,_,_,[],[],Board,Board).
+fillRestHints(Sum, IncompleteSum, CompleteHint,[NextElement|Rest], [[I, J]|RestHint], Board, NewBoard) :-
+    index(Board, I, J, NextElement),
+    nonvar(NextElement) ->                               %this element has been generated before
+    not(member(NextElement, CompleteHint)),
+    NewSum is IncompleteSum + NextElement,
+    Sum >= NewSum,
+    fillRestHints(Sum, NewSum, [CompleteHint,NextElement],Rest, RestHint, Board, NewBoard);
+    (Rest = [] ->                                 %last element of the hint
     %if
     NextElement is Sum - IncompleteSum,          %try to unify with rest to sum
     substituteNumber(NextElement),
-    not(member(CompleteHint, NextElement));
+    not(member(NextElement, CompleteHint)),
+    insertToMatrix(NextElement, Board, NewBoard, I, J);
     %else
     substituteNumber(NextElement),
-    not(member(CompleteHint, NextElement)),
+    not(member(NextElement, CompleteHint)),
     NewSum is IncompleteSum + NextElement,
     Sum > NewSum,
-    fillRestHints(Sum, NewSum, CompleteHint, Rest).
-
-% Iterates through List of substituted values and given Hints with
-% coordinates, and inserts values from List to Board.
-unifyBoard([],[],Board, Board).
-unifyBoard([[_|Filled]|List], [[_|Hint]|Rest], Board, NewBoard) :- %first values are Sums which doesn't matter at that point
-    unifyHint(Filled, Hint, Board, CurBoard),
-    unifyBoard(List, Rest,CurBoard, NewBoard).
-
-unifyHint([],[], Board, Board).
-unifyHint([Value|RestList], [[I,J]|Rest], Board, NewBoard) :-
-    insertToMatrix(Value, Board, CurBoard, I, J),
-    unifyHint(RestList, Rest, CurBoard, NewBoard).
+    insertToMatrix(NextElement, Board, CurBoard, I, J),
+    fillRestHints(Sum, NewSum, [CompleteHint,NextElement], Rest, RestHint, CurBoard, NewBoard)).
 
 %final method, checks all invariants and prints output
 solve(IndexOfProblem, Board) :-
@@ -151,15 +154,15 @@ isSet(List) :-
   length(List, Length).
 
 %Used for substitution of numbers to cells
-substituteNumber(1).
-substituteNumber(2).
-substituteNumber(3).
-substituteNumber(4).
-substituteNumber(5).
-substituteNumber(6).
-substituteNumber(7).
-substituteNumber(8).
 substituteNumber(9).
+substituteNumber(8).
+substituteNumber(7).
+substituteNumber(6).
+substituteNumber(5).
+substituteNumber(4).
+substituteNumber(3).
+substituteNumber(2).
+substituteNumber(1).
 
 % +Indices, +Board, -List get a list of values at given indices of Board
 getListFromIndices(Board, Indices, List) :-
@@ -178,7 +181,8 @@ getListFromIndices(Board, Indices, List, NewList):-
 insertToMatrix(Value, Matrix, NewMatrix, X, Y) :-
     inserto(Value, Matrix, NewMatrix, Y, X).
 
-%following is a net code for inserting element to Matrix at X, Y :)
+%following is a net code for inserting element to Matrix at X, Y
+%source: http://stackoverflow.com/questions/35069340/insert-element-into-a-2d-list-in-prolog
 inserto(_,[],[],_,_).
 inserto(E,[[_|Xs]|Ys],[[E|Xs1]|Ys1],1,1) :-
                     !,inserto(E,[Xs|Ys],[Xs1|Ys1],0,0).
